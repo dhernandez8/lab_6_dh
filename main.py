494ad7d57ee8a13eb175a20ab3a2a66502e37056
oliver <useyourshadow@gmail.com>
2023-10-25 14:22:47 -0400

    added decode function :)

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 def encode(password):
     return int(password) + int('3'*len(password))
 
-
+#Oliver's Decode
+def decode(password):
+    list= []
+    decode = ''
+    for i in str(password):
+        list.append(i)
+    for digit in list:
+        if digit == '1':
+            decode += '8'
+        elif digit == '2':
+            decode += '9'
+        elif digit == '0':
+            decode +='7'
+        else:
+            decode += str(int(digit)-3)
+    return decode
 def main():
     password = ''
     while True:
@@ -16,6 +31,8 @@ def main():
         if user_action == '1':
             password = encode(input(f"Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
+        if user_action == '2':
+            print("The encoded password is", password, "and the original password is" , decode(password))
         if user_action == '3':
             break
 
